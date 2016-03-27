fa6b1b1fe4c92b789c986e4f9dec74d90c8e31f0
Ross Freeman <rfreeman1@me.com>
2016-03-27 11:30:23 -0400

    Initial Commit

---
+++ FitAI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FitAI
+//
+//  Created by Ross Freeman on 3/27/16.
+//  Copyright © 2016 Ross Freeman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FitAI/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FitAI
+//
+//  Created by Ross Freeman on 3/27/16.
+//  Copyright © 2016 Ross Freeman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
